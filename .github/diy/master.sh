9fbb02d0d84c8cbe1b67b85c003319685f15d771
lllrrr <31834680+lllrrr@users.noreply.github.com>
2025-05-16 11:36:33 +0800

    Update master.sh

--- .github/diy/master.sh
+++ .github/diy/master.sh
@@ -79,6 +79,7 @@ git_sparse_clone openwrt-24.10 "https://github.com/openwrt/packages" "24packages
 sed -i 's/default LIBCURL_MBEDTLS/default LIBCURL_OPENSSL/g' curl/Config.in
 git_sparse_clone master "https://github.com/coolsnowwolf/packages" "ledepack" net/frp
 git_sparse_clone master "https://github.com/coolsnowwolf/luci" "ledeluci" applications/luci-app-autoreboot applications/luci-app-frpc applications/luci-app-frps
+git_sparse_clone other "https://github.com/Lienol/openwrt-package" "lienol" lean/luci-app-autoreboot
 
 sed -i \
 -e 's?include \.\./\.\./\(lang\|devel\|net\|utils\)?include $(TOPDIR)/feeds/packages/\1?' \
